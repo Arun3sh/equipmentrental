190ecf762ae5a17cebdc2227643e781e0da166b9
Arun3sh <m.aruneshwar@gmail.com>
2022-02-05 16:09:42 +0530

    other update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Navitem from './components/Navbar';
+import Homepage from './components/Homepage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navitem />
+			<Switch>
+				<Route exact path="/">
+					<Homepage />
+				</Route>
+				<Route path="/product">Product</Route>
+				<Route patyh="/checkout">checkout</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
