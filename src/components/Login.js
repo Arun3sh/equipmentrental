95641b875c5ba77fb31ea0763764164c351091b9
Arun3sh <m.aruneshwar@gmail.com>
2022-02-19 23:54:14 +0530

    email based login

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,14 +13,14 @@ toast.configure();
 function Login() {
 	const history = useHistory();
 	const { setLogin, setIsAdmin } = useContext(authContext);
-	const [username, setUserName] = useState(null);
+	const [email, setEmail] = useState(null);
 	const [password, setPassword] = useState(null);
 
 	const checkUser = async () => {
 		await fetch(`${API}/users/login`, {
 			method: 'POST',
 			body: JSON.stringify({
-				username,
+				email,
 				password,
 			}),
 			headers: {
@@ -29,70 +29,77 @@ function Login() {
 		})
 			.then((data) => data.json())
 			.then((userToken) => {
-				localStorage.setItem('token', userToken.token);
-				setLogin(true);
-				if (username === 'admin') {
+				if (email === 'admin@real.com') {
 					setIsAdmin(true);
 				}
-				toast.success('Loged in');
-				// <ToastContainer />;
+
 				history.push('/products');
+
+				localStorage.setItem('token', userToken.token);
+				setLogin(true);
+
+				toast.success('Loged in');
 			})
 			.catch(() => {
 				toast.error('Invalid login attempt');
-				// alert('user name / email invalid');
 			});
 	};
 
 	return (
-		<div className="login-wrapper">
-			{/* Illustration Container */}
-			<div className="row-illustration">
-				<img src={illu} alt="Illustration" aria-label="Think image" />
+		<div className="container-sm login">
+			<div className="logo-title">
+				<h2>REAL</h2>
+				<p>Rental Equipment in All Locations</p>
 			</div>
+			<div className="login-wrapper">
+				{/* Illustration Container */}
+				<div className="row-illustration">
+					<img src={illu} alt="Illustration" aria-label="Think image" />
+				</div>
 
-			{/* Login Container */}
-			<div className="row-login">
-				<div className="login-heading">
-					<h2>Login</h2>
-				</div>
-				<div className="email-container">
-					<TextField
-						id="username"
-						name="username"
-						autoComplete="off"
-						autoFocus={true}
-						variant="outlined"
-						className="email-textfield"
-						label="Enter User Name"
-						onChange={(e) => setUserName(e.target.value)}
-					/>
-				</div>
-				<div className="password-container">
-					<TextField
-						id="password"
-						name="password"
-						variant="outlined"
-						className="password-textfield"
-						type="password"
-						label="Enter Password"
-						onChange={(e) => setPassword(e.target.value)}
-						onKeyPress={(e) => e.key === 'Enter' && checkUser()}
-					/>
-				</div>
-				<div className="login-btn-container">
-					<div className="forgot-password">
-						<Link to="/forgot-password" aria-label="forgot password">
-							Forgot Password?
-						</Link>
+				{/* Login Container */}
+				<div className="row-login">
+					<div className="login-heading">
+						<h2>Login</h2>
 					</div>
-					<Button variant="outlined" onClick={checkUser}>
-						LOGIN
-					</Button>
-					<div className="register-yet">
-						<Link to="/register" aria-label="not registered">
-							Haven't Registered Yet?
-						</Link>
+					<div className="email-container">
+						<TextField
+							id="email"
+							name="email"
+							autoComplete="off"
+							autoFocus={true}
+							variant="outlined"
+							className="email-textfield"
+							label="Enter email id"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className="password-container">
+						<TextField
+							id="password"
+							name="password"
+							variant="outlined"
+							className="password-textfield"
+							type="password"
+							label="Enter Password"
+							onChange={(e) => setPassword(e.target.value)}
+							onKeyPress={(e) => e.key === 'Enter' && checkUser()}
+						/>
+					</div>
+					<div className="login-btn-container">
+						<div className="forgot-password">
+							<Link to="/forgot-password" aria-label="forgot password">
+								Forgot Password?
+							</Link>
+						</div>
+						<Button variant="outlined" onClick={checkUser}>
+							LOGIN
+						</Button>
+						<div className="register-yet">
+							<Link to="/register" aria-label="not registered">
+								Haven't Registered Yet?
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
