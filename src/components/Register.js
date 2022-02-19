640e70e9e1903829d01c24ba344cb54478de1725
Arun3sh <m.aruneshwar@gmail.com>
2022-02-19 23:54:41 +0530

    toast & header added

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,9 @@ import { Button, TextField } from '@mui/material';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { API } from '../assets/global';
+import { toast } from 'react-toastify';
+
+toast.configure();
 
 function Register() {
 	const history = useHistory();
@@ -16,8 +19,9 @@ function Register() {
 		}).then((res) => {
 			if (res.ok) {
 				history.push('/login');
+				toast.success('User Registered');
 			} else {
-				alert('user email already exists! try forgot password');
+				toast.error('user email already exists! try forgot password');
 			}
 		});
 	};
@@ -27,15 +31,14 @@ function Register() {
 		email: yup.string().email().required('email id is required'),
 		password: yup
 			.string()
-			.min(4, 'Min 4 characters')
-			.required('Min 4 characters')
+			.required('Min 8 characters')
 			.matches(
 				/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
 				'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character'
 			),
 		cpassword: yup
 			.string()
-			.required('Please confirm the password')
+			.required('Please re-enter the password')
 			.oneOf([yup.ref('password')], 'Passwords do not match'),
 	});
 	const { values, handleBlur, handleChange, handleSubmit, errors, touched } = useFormik({
@@ -49,68 +52,74 @@ function Register() {
 		onSubmit: () => register(),
 	});
 	return (
-		<div className="register-wrapper">
-			<h3>Create an Account</h3>
+		<div className="container-sm register">
+			<div className="logo-title">
+				<h2>REAL</h2>
+				<p>Rental Equipment in All Locations</p>
+			</div>
+			<div className="register-wrapper">
+				<h3>Create an Account</h3>
 
-			<form className="registeruser-form" onSubmit={handleSubmit} autoComplete="off">
-				<TextField
-					id="username"
-					name="username"
-					value={values.username}
-					label="User Name"
-					variant="outlined"
-					style={{ width: '40vh' }}
-					onChange={handleChange}
-					onBlur={handleBlur}
-					error={errors.username && touched.username}
-					helperText={errors.username && touched.username ? errors.username : ''}
-				/>
-				<TextField
-					id="outlined-basic"
-					name="email"
-					value={values.email}
-					label="Enter email id"
-					variant="outlined"
-					style={{ width: '40vh' }}
-					onChange={handleChange}
-					onBlur={handleBlur}
-					error={errors.email && touched.email}
-					helperText={errors.email && touched.email ? errors.email : ''}
-				/>
-				<TextField
-					id="password"
-					name="password"
-					value={values.password}
-					type="password"
-					label="Set Password"
-					variant="outlined"
-					style={{ width: '40vh' }}
-					onChange={handleChange}
-					onBlur={handleBlur}
-					error={errors.password && touched.password}
-					helperText={errors.password && touched.password ? errors.password : ''}
-				/>
-				<TextField
-					id="cpassword"
-					name="cpassword"
-					value={values.cpassword}
-					type="password"
-					label="Confirm Password"
-					variant="outlined"
-					style={{ width: '40vh' }}
-					onChange={handleChange}
-					onBlur={handleBlur}
-					error={errors.cpassword && touched.cpassword}
-					helperText={errors.cpassword && touched.cpassword ? errors.cpassword : ''}
-				/>
-				<Button variant="outlined" type="submit" color="success">
-					Submit
-				</Button>
-			</form>
-			<div className="user-already">
-				<Link to="/login" aria-label="login">
-					Already an user? Login
-				</Link>
+				<form className="registeruser-form" onSubmit={handleSubmit} autoComplete="off">
+					<TextField
+						id="username"
+						name="username"
+						value={values.username}
+						label="User Name"
+						variant="outlined"
+						style={{ width: '40vh' }}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.username && touched.username}
+						helperText={errors.username && touched.username ? errors.username : ''}
+					/>
+					<TextField
+						id="outlined-basic"
+						name="email"
+						value={values.email}
+						label="Enter email id"
+						variant="outlined"
+						style={{ width: '40vh' }}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.email && touched.email}
+						helperText={errors.email && touched.email ? errors.email : ''}
+					/>
+					<TextField
+						id="password"
+						name="password"
+						value={values.password}
+						type="password"
+						label="Set Password"
+						variant="outlined"
+						style={{ width: '40vh' }}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.password && touched.password}
+						helperText={errors.password && touched.password ? errors.password : ''}
+					/>
+					<TextField
+						id="cpassword"
+						name="cpassword"
+						value={values.cpassword}
+						type="password"
+						label="Confirm Password"
+						variant="outlined"
+						style={{ width: '40vh' }}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						error={errors.cpassword && touched.cpassword}
+						helperText={errors.cpassword && touched.cpassword ? errors.cpassword : ''}
+					/>
+					<Button variant="outlined" type="submit" color="success">
+						Submit
+					</Button>
+				</form>
+				<div className="user-already">
+					<Link to="/login" aria-label="login">
+						Already an user? Login
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
