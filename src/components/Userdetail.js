762249431a3860decfa655a2f323061429a65d4e
Arun3sh <m.aruneshwar@gmail.com>
2022-03-01 23:10:11 +0530

    user detail added

--- src/components/Userdetail.js
+++ src/components/Userdetail.js
@@ -35,17 +35,43 @@ function Userdetail() {
 
 		return (
 			<div className="container-sm user-detail">
-				<h5>{user.username}</h5>
-				<p>{user.email}</p>
-				<br />
-				<h6>Recent Orders - Quantity</h6>
-				{user.orders !== undefined
-					? userOrder.map(({ pname, quantity }, index) => (
-							<li key={index}>
-								{pname} - {quantity}
-							</li>
-					  ))
-					: '0'}
+				<div className="userDetail-wrapper">
+					<div className="user-name">
+						<h5>User Name : </h5>
+						<p>{user.username}</p>
+					</div>
+					<div className="user-email">
+						<h5>User Email : </h5>
+						<p>{user.email}</p>
+					</div>
+					<br />
+					<h6>Recent Orders - Quantity</h6>
+					{user.orders !== undefined
+						? // To get the user orders and display all the rented items
+						  userOrder.map((list) =>
+								list
+									.filter((e) => e.pid === undefined)
+									.map((d, index) => (
+										<div className="order-listed" key={index}>
+											<div className="user-order">
+												<h5>Order ID : </h5>
+												<p>{d}</p>
+											</div>
+
+											<ul>
+												{list
+													.filter((e) => e.pid !== undefined)
+													.map(({ pname, quantity }, index) => (
+														<li key={index}>
+															{pname} - {quantity}
+														</li>
+													))}
+											</ul>
+										</div>
+									))
+						  )
+						: '0'}
+				</div>
 			</div>
 		);
 	}
